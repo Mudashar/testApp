c79399fdba5650f1cef1f4d95e021f33fa6082bb
Mudashar uddin <Mudashar@users.noreply.github.com>
2023-09-02 15:40:09 +0530

    port changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.listen(3000);
+app.listen(process.env.port || port);
 app.set('view engine','ejs');
 app.get('/',(req,res)=>{
     res.render('index');
