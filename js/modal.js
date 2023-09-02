521967b10121449f1bca6416b31125fb88cee967
Mudashar uddin <Mudashar@users.noreply.github.com>
2023-09-02 15:37:09 +0530

    money manager

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+const myModal = document.getElementById('exampleModal')
+const myInput = document.getElementById('edit')
+const myModal2 = document.getElementById('exampleModal2')
+const myInput2 = document.getElementById('edit2')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+myModal2.addEventListener('shown.bs.modal', () => {
+    myInput2.focus()
+  })
