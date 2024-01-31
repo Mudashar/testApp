127f52661cb49d0c6c34eea7c473bc5514c64c23
Mudashar uddin <mudashar@users.noreply.github.com>
2024-02-01 01:42:34 +0530

    Added Recent transaction features.

--- getDboard.js
+++ getDboard.js
@@ -1,6 +1,17 @@
 const Dboard = require('./models/dboard');
+const Mlog = require('./models/mlog');
+const Tlog = require('./models/tlog');
+const Income = require('./models/income');
 
-const getDboard = (res,alert) => {
+const getDboard = async(res,alert) => {
+    const logs = await Mlog.aggregate([
+        { $unionWith: { coll: "incomes"} },
+        { $unionWith: { coll: "tlogs"} },
+        { $sort: { updatedAt: -1}},
+        { $limit: 10 }
+     ]);
+     //res.status(200).send({data: logs});
+     //console.log(logs);
     var queryId = {_id: '64f4ec4a3581c26a9032a37b'};
     Dboard.findOne(queryId)
     .then(r => {
@@ -8,10 +19,14 @@ const getDboard = (res,alert) => {
         var rem = 0;
         if(x > 1000){
             rem = x % 1000;
-            x = x - rem + 1000;
-            rem = 1000 - rem;
-        }       
-        res.render('index',{alert: alert, mutual: x, neg: rem ,actual: r.mutual, travel: r.travel});
+            if(rem > 0){
+                x = x - rem + 1000;
+                rem = 1000 - rem;
+            }
+        } 
+        
+        res.render('index',{alert: alert, mutual: x, neg: rem ,actual: r.mutual, travel: r.travel, logs: logs});       
+        
     })
     .catch(e => console.log(e)); 
 }
